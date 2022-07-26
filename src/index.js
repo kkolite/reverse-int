84813ad44f35d70b943546a610bd2b7cbc520c8a
kkolite <litavoregor@mail.ru>
2022-07-26 13:56:14 +0300

    Elona

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+  let str = String(n);
+  let m = 0
+  if (str[0] == '-') {
+    m = 1;
+  }
+  else (m = 0)
+  let arr = [];
+  for (i = m; i < str.length; i++) {
+    arr.push(str[i])
+  }
+
+  arr.reverse();
+  return arr.join('')
 }
